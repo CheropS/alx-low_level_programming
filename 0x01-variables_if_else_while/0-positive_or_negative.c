936d639c2b2761e8a0fe86a50b6d248e62756fc4
CheropS <sh.cherop@gmail.com>
2022-06-03 13:00:15 -0700

    edit arithmetic operation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
+
 /* 
- * Main: entry point
- * Description: Prints a random number either positive,zero, or negative 
+ * Main: main entry point
+ * Description: Prints - a random number either positive,zero, or negative 
  * Return: 0 if success. 
  */
 
 /* betty style doc for function main goes there */
+
 int main(void)
+
 {
 		int n;
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
+
 				if (n == 0)
 					printf("%d is zero\n", n);
 				else if (n > 0)
@@ -20,5 +24,5 @@ int main(void)
 				else
 					printf("%d is negative\n", n);
 					/* your code goes there */
-					return (0);
+				return (0);
 }
