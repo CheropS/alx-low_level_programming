12803c91dd55710db6040d20381fdb364224495d
CheropS <sh.cherop@gmail.com>
2022-06-03 14:30:08 -0700

    alphabet in lower case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lower case
+ * followed by a new line
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
