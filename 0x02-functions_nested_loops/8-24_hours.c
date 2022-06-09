18e363d27092f7d034ff182851bc598ec597c842
CheropS <sh.cherop@gmail.com>
2022-06-09 05:15:58 -0700

    edit spacing errors

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 	int x;
 	int y;
 
-	for (x = 0; x <=23; x++)
+	for (x = 0; x <= 23; x++)
 {
-	for (y = 0; y <=59; y++)
+	for (y = 0; y <= 59; y++)
 	{
 		_putchar (x / 10 + '0');
 		_putchar (x % 10 + '0');
 		_putchar (':');
-		_putchar (y /10 + '0');
+		_putchar (y / 10 + '0');
 		_putchar (y % 10 + '0');
 		_putchar ('\n');
 	}
