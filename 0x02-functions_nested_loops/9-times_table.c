5d7db1c3a95867069ae14d740d5c9bfc64aa346e
CheropS <sh.cherop@gmail.com>
2022-06-09 05:28:58 -0700

    prints multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table starting with 0
+ * Return: 0 if successful
+ */
+
+void times_table(void)
+{
+	int row, col;
+	int mult = 0;
+
+	for (row = 0; row < 10; col++)
+	{
+		mult = (row * col);
+		if (mult < 10)
+		{
+			if (col != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			_putchar(mult + '0');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar((mult / 10 + '0'));
+			_putchar((mult % 10) + '0');
+		}
+		if (col != 9)
+			_putchar(',');
+	}
+	_putchar('\n');
+}
