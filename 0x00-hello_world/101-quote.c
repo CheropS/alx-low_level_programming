8a99bb5686d7af2b3c56180aaa0846d93de84834
CheropS <sh.cherop@gmail.com>
2022-06-09 02:20:39 -0700

    new directories

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Main: main - Entry point
+ * Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
+ * Return: 1 if successful. 
+ */
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
