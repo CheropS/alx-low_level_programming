0847191522ebd58f3b815cf48edd17415b13393d
CheropS <sh.cherop@gmail.com>
2022-06-10 02:32:48 -0700

    size of a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line
+ *@size: the size of a square
+ * Return: always 0
+ */
+
+void print_square(int size)
+{
+	int sq1, sq2;
+
+	if (size > 0)
+	{
+		for (sq1 = 0; sq1 < size; sq1++)
+		{
+			for (sq2 = 0; sq2 < size; sq2++)
+				_putchar('#');
+			if (sq1 == size - 1)
+				continue;
+			_putchar('\n')
+		}
+	}
+
+	_putchar('\n');
+}
