172404646ef50314680683b7f49b7ed742417150
CheropS <sh.cherop@gmail.com>
2022-06-10 02:08:20 -0700

    changing code lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,16 @@ void print_line(int n)
 {
 	int len;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		for (len = 0; len < n; len++)
-			_putchar('_');
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
 	}
-
-	_putchar('n');
 }
