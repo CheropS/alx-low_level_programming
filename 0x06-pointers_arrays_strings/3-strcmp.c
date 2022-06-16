a9f8a1113b3843862d9d6ede9f69a8e3a34b361d
CheropS <sh.cherop@gmail.com>
2022-06-16 16:08:41 +0300

    removed char def

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,13 +6,11 @@
  * _strcmp - compares two strings
  * s1: first integer
  * s2: second integer
- * Return: Always 0.
+ * Return: if str1 == str2, 0.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-    int s1; s2;
-
     while (*s1 && *s2 && *s1 == *s2)
     {
         s1++;
