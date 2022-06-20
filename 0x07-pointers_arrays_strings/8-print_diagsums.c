97c0aeb34e9584658a5b544aa1b1e95e8ed132fc
CheropS <sh.cherop@gmail.com>
2022-06-20 18:27:43 +0000

    changed sum1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,21 +12,16 @@
 void print_diagsums(int *a, int size)
 {
 	int w = 0;
-	int x = size - 1;
 	int sum1 = 0;
 	int sum2 = 0;
-
-	while (w <= (size * size))
+	
+	while (w < size)
 	{
-		sum1 = sum2 + a[w];
-		w = w + size + 1;
+		sum1 = sum1 + *(a + w * size + w);
+		sum2 = sum2 + *(a + w * size + size - w - 1);
+		
+		w++;
 	}
-
-	while (x < (size * size - 1))
-	{
-		sum2 += a[x];
-		x = x + size - 1;
-	}
-
+	
 	printf("%d, %d\n", sum1, sum2);
 }
