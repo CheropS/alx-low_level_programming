8e5150f3244e857f426c2e17f19feafab152b466
CheropS <sh.cherop@gmail.com>
2022-06-20 18:30:04 +0000

    add line after decl

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,14 +14,12 @@ void print_diagsums(int *a, int size)
 	int w = 0;
 	int sum1 = 0;
 	int sum2 = 0;
-	
+
 	while (w < size)
 	{
 		sum1 = sum1 + *(a + w * size + w);
 		sum2 = sum2 + *(a + w * size + size - w - 1);
-		
 		w++;
 	}
-	
 	printf("%d, %d\n", sum1, sum2);
 }
