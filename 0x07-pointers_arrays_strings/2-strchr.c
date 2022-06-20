19a2bd23180e48480553a5a8407d761391199d91
CheropS <sh.cherop@gmail.com>
2022-06-20 16:12:16 +0000

    function that locates a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a char in a string
+ * @s: string
+ * @c: character
+ * Return: pointer to first occurence of c in string s or NULL if c not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
