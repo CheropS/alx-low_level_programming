4bd7f570b2e0405940029600ad81ca12bd0b135f
CheropS <sh.cherop@gmail.com>
2022-06-23 13:22:23 +0000

    prints new string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string followed by new line
+ * @s: character s
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+This directory contains functions for recursions
+
+0-puts_recursion.c - contains function that prints a string
