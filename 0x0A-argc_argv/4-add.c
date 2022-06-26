e426561789d0bb14e4b028c75878af3b490fa7b0
CheropS <sh.cherop@gmail.com>
2022-06-26 19:05:58 +0000

    adds two digits

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - programs that adds positive numbers
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: always 0, error return 1.
+ */
+
+int main(argc, char *argv[])
+{
+	int a, digit, sum = 0;
+
+	for (digit = 0; argv[a][digit]; digit++)
+	{
+		if (argv[a][digit] < '0' || argv[a][digit] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	sum += atoi(argv[a]);
+}
