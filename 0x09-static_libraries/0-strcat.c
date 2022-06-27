d681ddd3e8e978246ac2be3c89b289121b9b4df2
CheropS <sh.cherop@gmail.com>
2022-06-27 17:59:06 +0000

    creating static files

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
