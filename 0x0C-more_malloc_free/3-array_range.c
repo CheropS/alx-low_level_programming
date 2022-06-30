d58eae5541980d72a4bd3b3cadf1b39266de7f82
CheropS <sh.cherop@gmail.com>
2022-06-30 10:19:45 +0000

    create array of strings

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *array_range - function that creates arrays of int
+ * @max: maximum value of integer
+ * @min: minimum value of integer
+ * Return: min > max, malloc fails - NULL, otherwise array.
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, base, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (base = 0; base < size; base++)
+		array[base] = min++;
+
+	return (array);
+}
