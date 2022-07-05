ee8c1ce8df2c0b998a951fbab2f72f66ef4fc4ec
CheropS <sh.cherop@gmail.com>
2022-07-05 10:54:50 +0000

    new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,11 +17,11 @@ struct dog
 };
 
 
-typedef struct dog dog_g;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_g *new_dog(char *name, float age, char *owner);
-void free_dog(dog_g *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
