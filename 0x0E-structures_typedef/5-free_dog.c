a4b05c0b3ee3c3653617660ab692478116907fbf
CheropS <sh.cherop@gmail.com>
2022-07-05 11:59:45 +0000

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: input for dog
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
