7ff593b98e334d2802c184c76021543c9dc3af5d
CheropS <sh.cherop@gmail.com>
2022-07-05 10:44:46 +0000

    syntax error fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,6 +5,7 @@
 /**
  * print_dog - prints struct dog
  * @d: dog to be initialized
+ * Return: void
  */
 
 void print_dog(struct dog *d)
@@ -15,13 +16,10 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age < 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n);
+		printf("Owner: (nil)\n");
 	else
-		printf("Owner : % s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
