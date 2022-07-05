501fead0395c4149de50950ed40bc830932c95e7
CheropS <sh.cherop@gmail.com>
2022-07-05 08:19:07 +0000

    more macros

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Description: computes sum of x and y
+ * @x: first number
+ * @y: second number
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
