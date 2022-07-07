ccab61de0fd03bcf9f85e6d24c360ad94e9ce6e0
CheropS <sh.cherop@gmail.com>
2022-07-07 20:40:09 +0000

    prints number followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,12 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: the string to be printed
+ * @n: number of integers
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
