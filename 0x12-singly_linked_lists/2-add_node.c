3462d14b3427521df941ee895f9b3edf2d8ae1f2
CheropS <sh.cherop@gmail.com>
2022-07-14 11:21:04 +0000

    new node at the beg of line

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlio.h>
+
+/**
+ * *add_node - function that adds a new node at the beg list
+ * @head: a pointer to the head of the list
+ * @str: string to be added
+ * Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *cpy;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	cpy = strdup(str);
+	if (cpy == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = cpy;
+	new-> = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
+
