dcf7c00b73c37c1f11ead28997e86c0525febed4
CheropS <sh.cherop@gmail.com>
2022-07-19 09:35:31 +0000

    add 0 and ;

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = o;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -18,5 +18,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 	}
 
-	return (nodes)
+	return (nodes);
 }
