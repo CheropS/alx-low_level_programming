6e1b4a2eac0ccc82a605873f2754554822bdab42
CheropS <sh.cherop@gmail.com>
2022-07-22 10:29:44 +0000

    edit code removed pointer equals to

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,18 +14,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, base = 1;
 	int len;
 
-	if (w == '\0')
-		return (0);
-
-	for (len = 0; w[len];)
+	for (len = 0; b[len];)
 		len++;
 
 	for (len -= 1; len >= 0; len--)
 	{
-		if (w[len] != '0' && w[len] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
-		num += (w[len] - '0') * base;
+		num += (b[len] - '0') * base;
 		base *= 2;
 	}
 
