334ce0ea040591ed2ce7fb3e51287fcbb12858fb
CheropS <sh.cherop@gmail.com>
2022-07-25 13:09:27 +0000

    write function for read and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads text file and prints out
+ * @filename: name of the file
+ * @letters: number of letters to be printed and read by func
+ * Return: number of letters to be printed, otherwise 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize o, r, w;
+	char *base;
+
+	if (filename == NULL)
+		return (0);
+
+	base = malloc(sizeof(char) * letters);
+	if (base == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, base, letters);
+	w = write(STDOUT_FILENO, base, r);
+
+	if (o == -1 || r == -1 || w != r)
+	{
+		free(base);
+		return (0);
+	}
+
+	free(base);
+	close(o);
+
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+The folder contains I/O functions
+open() function for opening a file
+write() executable function to write on a file
+close() function to close a file
+
