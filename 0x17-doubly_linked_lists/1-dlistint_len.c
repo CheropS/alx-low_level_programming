3387d3b0dfc56dc72138bbd59153a5d4c801cb02
CheropS <sh.cherop@gmail.com>
2022-09-02 19:21:30 +0000

    counts the number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_t - return the number of elements in a linked list
+ * @h: the head of the dlistint_t list
+ * Return: number of elements in a list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
